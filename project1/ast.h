c0613b4387dc5336e2b36bebfbefc6a6ae1f972e
Longzhao <49816418+VinsonSun@users.noreply.github.com>
2020-10-17 22:53:29 +0800

    Update ast.h

--- project1/ast.h
+++ project1/ast.h
@@ -5,17 +5,17 @@ extern char* yytext;
 
 typedef struct ASTNode {
     int lineNum;
-    char* name;
+    char *name;
     int seqNum;
     struct ASTNode *child[CHILD_MAX_NUM];
     union val{
         int intVal;
         float floatVal;
-        char* stringVal;
+        char *stringVal;
     }val;
 }ASTNode;
 ASTNode *root;
-ASTNode* newASTNode(int lineNum, char* name, void *val);
+ASTNode *newASTNode(int lineNum, char* name, void *val);
 void addChild(ASTNode *root, ASTNode *leaf);
 void printAST();
 #endif
