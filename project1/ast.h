bc493eb897f79e5426db64c19355f83ad6ff4b8f
Longzhao <49816418+VinsonSun@users.noreply.github.com>
2020-10-18 19:08:45 +0800

    Update ast.h

--- project1/ast.h
+++ project1/ast.h
@@ -1,7 +1,7 @@
 #ifndef AST
 #define AST
 #define MAX_CHILD 10
-typedef struct Node
+typedef struct ASTNode
 {
     int line;
     int type;
@@ -9,12 +9,12 @@ typedef struct Node
     int childNum;
     struct Node *child[MAX_CHILD];
     union Val{
-        int i;
-        float f;
-        char *s;
+        int intVal;
+        float floadVal;
+        char *stringVal;
     }val;
-}Node;
-Node *root;
-Node *new_node(int line, char *nodeName, int type, void *val, int childNum, ...);
+}ASTNode;
+ASTNode *root;
+ASTNode *newASTNode(int line, char *nodeName, int type, void *val, int childNum, ...);
 void traverse(Node *t, int spaceNum);
 #endif
