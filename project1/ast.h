2da4b55cc7f8b7e81f81ca03dce610f727c7c72c
Longzhao <49816418+VinsonSun@users.noreply.github.com>
2020-10-18 17:13:59 +0800

    Update ast.h

--- project1/ast.h
+++ project1/ast.h
@@ -18,5 +18,5 @@ typedef struct Node
 }Node;
 Node *root;
 Node *new_node(int line, char *nodeName, int type, void *val, int childNum, ...);
-void traverse();
+void traverse(Node *t, int spaceNum);
 #endif
