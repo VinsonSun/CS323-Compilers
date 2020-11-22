c8f6c5a01d13ee12a08317b4949dda89e93c42fe
[VinsonSun] <[11710511@mail.sustc.edu.cn]>
2020-11-22 14:00:25 +0800

    updates files

---
+++ project2/ast.h
@@ -0,0 +1,21 @@
+#ifndef AST
+#define AST
+#define MAX_CHILD 10
+
+typedef struct ASTNode
+{
+    int line;
+    int type;
+    char *name;
+    int childNum;
+    struct ASTNode *child[MAX_CHILD];
+    union Val{
+        int intVal;
+        float floatVal;
+        char *stringVal;
+    }val;
+}ASTNode;
+ASTNode *root;
+ASTNode *newASTNode(int line, char *nodeName, int type, void *val, int childNum, ...);
+void traverse(ASTNode *t, int spaceNum);
+#endif
