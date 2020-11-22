6894844ff8b600b28f874551eba3394e52e2128e
Longzhao <49816418+VinsonSun@users.noreply.github.com>
2020-11-22 22:23:29 +0800

    Update semantic.h

--- project2/semantic.h
+++ project2/semantic.h
@@ -2,65 +2,10 @@
 #define SEMANTIC_H
 #define HASH_TABLE_SIZE 0x3fff
 
-enum{
-    INT_TYPE, FLOAT_TYPE, CHAR_TYPE
-};
-
-typedef struct Type{
-    enum{BASIC, ARRAY, STRUCTURE} kind;
-    union{
-        int basic;
-        struct{
-            Type* elem;
-            int size;
-        }array;
-        FieldList* structure;
-    }u;
-};
-
-typedef struct FieldList{
-    char* name;
-    Type* type;
-    FieldList *next;
-};
-
-typedef struct Var_hash_node
-{
-    char *name;
-    int line;
-    int depth;
-    Type *type;
-    struct _Operand *op;
-    Var_hash_node *next;
-    Var_hash_node *last;
-};
-
-typedef struct Type_node{
-    Type *type;
-    Type_node *next;
-    char *name;
-};
-
-typedef struct Func_hash_node{
-    char *name;
-    int line;
-    int whether_dec;
-    int whether_def;
-    Func_hash_node *next;
-    Func_hash_node *last;
-    Type *return_type;
-    Type_node* para_type_list;
-};
-
-typedef struct Var_list_node{
-    Var_hash_node *node;
-    Var_list_node *next;
-};
+#include "type.h"
 
 typedef struct ASTNode ASTNode;
 
-Var_hash_node *var_hash_table[HASH_TABLE_SIZE + 1];
-Func_hash_node *func_hash_table[HASH_TABLE_SIZE + 1];
 void semantic_func();
 Var_hash_node* get_var_hash_node(char *key);
 Func_hash_node* get_func_hash_node(char *key);
